a970361bc4a44b116230ba5d4635023e7a7069ab
Sandra Widmark <sandra.widmark@gmail.com>
2015-12-13 22:51:24 +0100

    added a lightbox with jquery

--- b5_infographic/js/scripts.js
+++ b5_infographic/js/scripts.js
@@ -178,3 +178,48 @@ $('.showme').click(function (e) {
     $('.third-section-facts, .globe-container').show();
     $(window).scrollTop($('.third-section-facts').offset().top);
 });
+
+
+
+
+//jquery - lightbox
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>"); 
+var $caption = $("<p></p>"); 
+
+//add image to overlay
+
+$overlay.append($image); 
+
+
+//A caption to overlay
+$overlay.append($caption); 
+
+//add overlay
+$("body").append($overlay);
+
+//capture click event on a link to an image 
+
+$("#imageGallery a").click(function(event){
+    event.preventDefault();
+  var imageLocation =  $(this).attr("href");
+    
+$image.attr("src", imageLocation); 
+
+
+
+    $overlay.show(); 
+
+
+//get childs attribute and set caption
+
+var captionText = $(this).children("img").attr("alt");
+$caption.text(captionText); 
+
+});
+
+$overlay.click(function(){
+
+$overlay.hide(); 
+}); 
