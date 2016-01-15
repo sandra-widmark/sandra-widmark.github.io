90d09ef5af6db9078a71a4b04964f24c8eb07167
Sandra Widmark <sandra.widmark@gmail.com>
2016-01-15 13:17:02 +0100

    added functions to make the ball move

--- C2_Spel/js/game.js
+++ C2_Spel/js/game.js
@@ -11,15 +11,23 @@ var ball;
  //fix so that it scales to different screen sizes
 
 function preload() {
+
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = '#eee';
-     game.load.image('ball', 'img/ball.png');
+    game.load.image('ball', 'img/ball.png');
 }
 function create() {
+    //initialize the Arcade Physics engine
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
     ball = game.add.sprite(50, 50, 'ball');
+    game.physics.enable(ball, Phaser.Physics.ARCADE);
+    ball.body.velocity.set(150, 150);
 }
-function update() {}
+function update() {
+   
+}
 
 
