8bd2809eba110cd41540936a4468348c2aac8e6d
Sandra Widmark <sandra.widmark@gmail.com>
2016-01-15 12:11:45 +0100

    added the ball to the game

--- C2_Spel/js/game.js
+++ C2_Spel/js/game.js
@@ -1,22 +1,25 @@
-  
+
 
 //Generate the canvas element with phaser
 
-  var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
-      preload: preload, create: create, update: update
-    });
+var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
 
+//a variable for creating the mine worker
 
-  //fix so that it scales to different screen sizes
-    function preload() {
+var ball;
+
+ //fix so that it scales to different screen sizes
+
+function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = '#eee';
-    }
-
-
-    function create() {}
-    function update() {}
+     game.load.image('ball', 'img/ball.png');
+}
+function create() {
+    ball = game.add.sprite(50, 50, 'ball');
+}
+function update() {}
 
 
