9d4ac66cb5ee2f5f714eb6dba3aaf8dcaab6615b
Sandra Widmark <sandra.widmark@gmail.com>
2016-02-02 22:27:43 +0100

    changed position of the js file

---
+++ C3_API/js/booksearch.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+/* Exempel från treehouse 
+
+$(document).ready(function () {
+  $('button').click(function (){
+    $("button").removeClass("selected"); //tar bort selected-class på de knappar som inte klickas på. 
+    $(this).addClass("selected"); //this refererar till "button" som klickas på, ingen av de andra knapparna. 
+    
+    var flickrAPI = "http://api.flickr.com/.....?jsoncallback=?"; //den sista json callback är mkt viktig att lägga till!
+    var animal = $(this).text(); //"this" i det här fallet refererar till knappen för det specifika djuret som klickas på. "text" tar all text från inuti html-elementet
+    var flickrOptions = {
+      tags : animal,
+      format : "json"
+  }; 
+    
+    function displayPhotos(data) { //data representerar json-datan som returneras av jquery
+      var photoHTML = '<ul>';
+      $.each( data.items, function (i, photo) {
+        photoHTML += '<li class="grid">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      
+      }); 
+      photoHTML += '</ul>'; 
+      $('#photos').html(photoHTML); 
+    
+    }
+    
+    
+    $.getJSON(flickrAPI, flickrOptions, displayPhotos); 
+ });
+  
+}); //avslut på document-ready funktionen. 
+
+*/
